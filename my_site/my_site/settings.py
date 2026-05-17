f2574896e282f5dc0b85a19687c19790b006bff6
Firdavs-cpu <firdavssaidjalalov73@gmail.com>
2026-05-17 22:51:19 +0300

    Настройте URL-адрес базы данных для развертывания Render

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -1,6 +1,7 @@
 
 from pathlib import Path
 import os
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv() 
@@ -78,6 +79,9 @@ DATABASES = {
 
     }
 }
+
+if os.getenv('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
